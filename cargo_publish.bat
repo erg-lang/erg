fd5e1c59ec30ec687d8ea62032ed3cfabd4bfb78
Shunsuke Shibayama <sbym1346@gmail.com>
2022-08-19 02:53:25 +0900

    Update cargo_publish.bat

--- cargo_publish.bat
+++ cargo_publish.bat
@@ -4,15 +4,15 @@ if %~dp0 == C:%homepath%\GitHub\erg\ (
     cd compiler/erg_common
     echo publish erg_common ...
     cargo publish
-    timeout 15
+    timeout 10
     cd ../erg_parser
     echo publish erg_parser ...
     cargo publish
-    timeout 15
+    timeout 10
     cd ../erg_compiler
     echo publish erg_compiler ...
     cargo publish
-    timeout 15
+    timeout 10
     cd ../../
     cargo publish
     echo completed
