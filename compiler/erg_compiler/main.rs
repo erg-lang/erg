1c29e21aa452fd9c98f87c57b108dbf4c03fabb1
Shunsuke Shibayama <sbym1346@gmail.com>
2022-09-11 10:09:42 +0900

    Fix #115, #133

--- compiler/erg_compiler/main.rs
+++ compiler/erg_compiler/main.rs
@@ -3,6 +3,7 @@ extern crate erg_compiler;
 extern crate erg_parser;
 
 use std::process;
+use std::thread;
 
 use erg_common::config::ErgConfig;
 use erg_common::traits::Runnable;
@@ -14,7 +15,7 @@ use erg_parser::ParserRunner;
 
 use erg_type::deserialize::Deserializer;
 
-fn main() {
+fn run() {
     let cfg = ErgConfig::parse();
     match cfg.mode {
         "lex" => {
@@ -33,5 +34,21 @@ fn main() {
             println!("invalid mode: {other}");
             process::exit(1);
         }
+    }
+}
+
+fn main() {
+    if cfg!(windows) {
+        const STACK_SIZE: usize = 4 * 1024 * 1024;
+
+        let child = thread::Builder::new()
+            .stack_size(STACK_SIZE)
+            .spawn(run)
+            .unwrap();
+
+        // Wait for thread to join
+        child.join().unwrap();
+    } else {
+        run();
     }
 }
--- compiler/erg_parser/main.rs
+++ compiler/erg_parser/main.rs
@@ -2,6 +2,7 @@ extern crate erg_common;
 extern crate erg_parser;
 
 use std::process;
+use std::thread;
 
 use erg_common::config::ErgConfig;
 use erg_common::traits::Runnable;
@@ -9,7 +10,7 @@ use erg_common::traits::Runnable;
 use erg_parser::lex::LexerRunner;
 use erg_parser::ParserRunner;
 
-fn main() {
+fn run() {
     let cfg = ErgConfig::parse();
     match cfg.mode {
         "lex" => {
@@ -22,5 +23,21 @@ fn main() {
             println!("invalid mode: {other}");
             process::exit(1);
         }
+    }
+}
+
+fn main() {
+    if cfg!(windows) {
+        const STACK_SIZE: usize = 4 * 1024 * 1024;
+
+        let child = thread::Builder::new()
+            .stack_size(STACK_SIZE)
+            .spawn(run)
+            .unwrap();
+
+        // Wait for thread to join
+        child.join().unwrap();
+    } else {
+        run();
     }
 }
