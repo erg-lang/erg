bf496aabb5936f50752f2a5d58e8ce2338274078
Cai Bingjun <1945458160@qq.com>
2022-10-01 15:31:28 +0800

    Improve translation of zh_CN and zh_TW

--- compiler/erg_common/help_messages.rs
+++ compiler/erg_common/help_messages.rs
@@ -10,7 +10,7 @@ USAGE:
 
 ARGS:
     <script> スクリプトファイルからプログラムを読み込む
-             <script>に渡す引数を入力する
+            <script>に渡す引数を入力する
 
 OPTIONS
     --help/-?/-h                         このhelpを表示
@@ -27,26 +27,26 @@ SUBCOMMAND
     -m mod : モジュールを実行",
 
     "simplified_chinese" =>
-        "\
+    "\
 USAGE:
     erg [OPTIONS] [SUBCOMMAND] [ARGS]...
-
+    
 ARGS:
-    <script> 從腳本文件中讀取的程序
-             參數也可以指定傳遞給 <script>
-
+    <script> 从脚本文件读取程序
+            参数也可以指定要传递给 <script>
+    
 OPTIONS
-    --help/-?/-h                         显示此帮助
+    --help/-?/-h                         显示帮助
     --version/-V                         显示版本
-    --verbose 0|1|2                      详细程度
-    --opt-level/-o 0|1|2|3               优化级别
+    --verbose 0|1|2                      指定细致程度
+    --opt-level/-o 0|1|2|3               指定优化级别
     --python-version/-p (uint 32 number) Python版本
     --py-server-timeout (uint 64 number) Python REPL服务器超时
-    --dump-as-pyc                        转储为 .pyc 文件
+    --dump-as-pyc                        转储为.pyc文件
     --mode lex|parse|compile|exec        执行模式
-
+    
 SUBCOMMAND
-    -c cmd : 作为字符串传入的程序
+    -c cmd : 作为字符串传入程序
     -m mod : 要执行的模块",
 
     "traditional_chinese" =>
@@ -55,21 +55,21 @@ USAGE:
     erg [OPTIONS] [SUBCOMMAND] [ARGS]...
 
 ARGS:
-    <script> 从脚本文件中读取的程序
-             参数也可以指定传递给 <script>
+    <script> 從腳本檔案讀取程式
+            參數也可以指定要傳遞給 <script>
 
 OPTIONS
-    --help/-?/-h                         顯示此幫助
+    --help/-?/-h                         顯示幫助
     --version/-V                         顯示版本
-    --verbose 0|1|2                      詳細程度
-    --opt-level/-o 0|1|2|3               優化級別
+    --verbose 0|1|2                      指定細緻程度
+    --opt-level/-o 0|1|2|3               指定優化級別
     --python-version/-p (uint 32 number) Python 版本
     --py-server-timeout (uint 64 number) Python REPL 服務器超時
     --dump-as-pyc                        轉儲為 .pyc 文件
     --mode lex|parse|compile|exec        執行模式
 
 SUBCOMMAND
-    -c cmd : 作為字符串傳入的程序
+    -c cmd : 作為字串傳入程式
     -m mod : 要執行的模塊",
 
     "english" =>
@@ -79,7 +79,7 @@ USAGE:
 
 ARGS:
     <script> program read from script file
-             Arguments can also be specified to be passed to the <script>
+            Arguments can also be specified to be passed to the <script>
 
 OPTIONS
     --help/-?/-h                         show this help
@@ -131,72 +131,72 @@ exec
 read
     <filename>.pycをデシリアライズしダンプ",
 
-    "traditional_chinese" =>
-        "\
-USAGE:
-    erg --mode [lex | parse | lower | check | compile | exec | read] [SUBCOMMAND] [ARGS]...
-
-lex
-    从 <filename>.er、REPL 等接收输入，并标记文本
-    以 TokenStream 形式返回分析结果
-
-parse
-    执行lex，获取TokenStream，解析语法
-    对多个模式定义语句进行脱糖，通过匹配转换并返回 ast（抽象语法树）
-
-lower
-    执行 parse 得到 ast
-    解析名称，检查类型和推断，并返回 ast
-
-check
-    执行lower并get ast
-    检查副作用、所有权和回报
-
-compile
-    運行檢查以檢查 ast
-    編譯 ast 並返回 <filename>.pyc
-
-exec
-    执行检查以检查 ast
-    执行 <filename>.pyc 后删除 <filename>.pyc
-
-read
-    反序列化 <filename>.pyc 并转储",
-
     "simplified_chinese" =>
-        "\
+    "\
 USAGE:
     erg --mode [lex | parse | lower | check | compile | exec | read] [SUBCOMMAND] [ARGS]...
 
 lex
-    從 <filename>.er、REPL 等接收輸入，並標記文本
-    以 TokenStream 形式返回分析結果
+    从 <filename>.er, REPL等接受输入, 并标记文本
+    以TokenStream形式返回分析结果
 
 parse
-    執行lex，獲取TokenStream，解析語法
-    對多個模式定義語句進行脫糖，通過匹配轉換並返回 ast（抽象語法樹）
+    执行lex, 获取TokenStream, 并解析语法
+    将多模式定义语句的语法糖按匹配转换并返回ast(抽象语法树)
 
 lower
-    執行 parse 得到 ast
-    解析名稱，檢查類型和推斷，並返回 ast
+    执行parse以获取ast
+    解析名称、检查类型和推断, 并返回ast
 
 check
-    執行lower並get ast
-    檢查副作用、所有權和回報
+    执行lower并获取ast
+    检查副作用、所有权并返回ast
 
 compile
-    运行检查以检查 ast
-    编译 ast 并返回 <filename>.pyc
+    运行check以获取检查完成的ast
+    编译ast并返回<文件名>.pyc
 
 exec
-    執行檢查以檢查 ast
-    執行 <filename>.pyc 後刪除 <filename>.pyc
+    运行check以获取检查完成的ast
+    在执行<filename>.pyc后删除<文件名>.pyc
 
 read
-    反序列化 <filename>.pyc 並轉儲",
+    反序列化<文件名>.pyc和dump",
+
+    "traditional_chinese" =>
+    "\
+USAGE:
+        erg --mode [lex | parse | lower | check | compile | exec | read] [SUBCOMMAND] [ARGS]...
+    
+lex
+    從<檔名>.er, REPL等接受輸入, 並標記文字
+    以TokenStream形式返回分析結果
+    
+parse
+    執行lex, 獲取TokenStream, 並解析語法
+    將多模式定義語句的語法糖按匹配轉換並返回ast(抽象語法樹)
+    
+lower
+    執行parse以獲取ast
+    解析名稱、檢查類型和推斷, 並返回ast
+    
+check
+    執行lower並獲取ast
+    檢查副作用、所有權並返回ast
+    
+compile
+    運行check以獲取檢查完成的ast
+    編譯ast並返回<檔名>.pyc
+    
+exec
+    運行check以獲取檢查完成的ast
+    在執行<檔名>.pyc後删除<檔名>.pyc
+    
+read
+    反序列化<檔名>.pyc和dump",
 
     "english" =>
-        "\
+    "\
 USAGE:
     erg --mode [lex | parse | lower | check | compile | exec | read] [SUBCOMMAND] [ARGS]...
 
