e02def52abee3d031f75059ef38f1c45516ee9a4
Shunsuke Shibayama <sbym1346@gmail.com>
2022-10-10 00:05:55 +0900

    Implement `Array/SetWithLength`

--- compiler/erg_compiler/std/_erg_std_prelude.py
+++ compiler/erg_compiler/std/_erg_std_prelude.py
@@ -4,5 +4,12 @@ def in_operator(x, y):
             return True
         # TODO: trait check
         return False
+    elif type(y) == list or type(y) == set:
+        # FIXME:
+        type_check = in_operator(x[0], y[0])
+        len_check = len(x) == len(y)
+        return type_check and len_check
+    elif type(y) == dict:
+        NotImplemented
     else:
         return x in y
