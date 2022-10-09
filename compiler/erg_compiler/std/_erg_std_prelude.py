701c1f2e407b5d32707374c655fb7d2825948603
Shunsuke Shibayama <sbym1346@gmail.com>
2022-10-10 00:18:04 +0900

    Update _erg_std_prelude.py

--- compiler/erg_compiler/std/_erg_std_prelude.py
+++ compiler/erg_compiler/std/_erg_std_prelude.py
@@ -4,12 +4,12 @@ def in_operator(x, y):
             return True
         # TODO: trait check
         return False
-    elif type(y) == list or type(y) == set:
+    elif (type(y) == list or type(y) == set) and type(y[0]) == type:
         # FIXME:
         type_check = in_operator(x[0], y[0])
         len_check = len(x) == len(y)
         return type_check and len_check
-    elif type(y) == dict:
+    elif type(y) == dict and type(y[0]) == type:
         NotImplemented
     else:
         return x in y
