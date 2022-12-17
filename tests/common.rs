8737de46bfcf9fcbf77973b9fc46572068701a47
Shunsuke Shibayama <sbym1346@gmail.com>
2022-12-17 22:08:25 +0900

    Split test code

---
+++ tests/common.rs
@@ -0,0 +1,83 @@
+#![allow(dead_code)]
+use std::path::PathBuf;
+
+use erg_common::config::ErgConfig;
+use erg_common::error::MultiErrorDisplay;
+use erg_common::python_util::PythonVersion;
+use erg_common::spawn::exec_new_thread;
+use erg_common::style::{GREEN, RESET};
+use erg_common::traits::{Runnable, Stream};
+
+use erg_compiler::error::CompileErrors;
+
+use erg::DummyVM;
+
+pub(crate) fn expect_success(file_path: &'static str) -> Result<(), ()> {
+    match exec_vm(file_path) {
+        Ok(0) => Ok(()),
+        Ok(i) => {
+            println!("err: end with {i}");
+            Err(())
+        }
+        Err(errs) => {
+            errs.fmt_all_stderr();
+            Err(())
+        }
+    }
+}
+
+pub(crate) fn expect_end_with(file_path: &'static str, code: i32) -> Result<(), ()> {
+    match exec_vm(file_path) {
+        Ok(0) => Err(()),
+        Ok(i) => {
+            if i == code {
+                Ok(())
+            } else {
+                println!("err: end with {i}");
+                Err(())
+            }
+        }
+        Err(errs) => {
+            errs.fmt_all_stderr();
+            Err(())
+        }
+    }
+}
+
+pub(crate) fn expect_failure(file_path: &'static str, errs_len: usize) -> Result<(), ()> {
+    match exec_vm(file_path) {
+        Ok(0) => Err(()),
+        Ok(_) => Ok(()),
+        Err(errs) => {
+            errs.fmt_all_stderr();
+            if errs.len() == errs_len {
+                Ok(())
+            } else {
+                println!("err: error length is not {errs_len} but {}", errs.len());
+                Err(())
+            }
+        }
+    }
+}
+
+/// The test is intend to run only on 3.11 for fast execution.
+/// To execute on other versions, change the version and magic number.
+fn _exec_vm(file_path: &'static str) -> Result<i32, CompileErrors> {
+    println!("{GREEN}[test] exec {file_path}{RESET}");
+    let mut cfg = ErgConfig::with_main_path(PathBuf::from(file_path));
+    cfg.py_command = if cfg!(windows) {
+        Some("python")
+    } else {
+        Some("python3")
+    };
+    // cfg.target_version = Some(PythonVersion::new(3, Some(10), Some(6))); // your Python's version
+    // cfg.py_magic_num = Some(3439); // in (most) 3.10.x
+    cfg.target_version = Some(PythonVersion::new(3, Some(11), Some(0)));
+    cfg.py_magic_num = Some(3495); // in 3.11.0
+    let mut vm = DummyVM::new(cfg);
+    vm.exec()
+}
+
+pub(crate) fn exec_vm(file_path: &'static str) -> Result<i32, CompileErrors> {
+    exec_new_thread(move || _exec_vm(file_path))
+}
--- tests/test.rs
+++ tests/test.rs
@@ -1,15 +1,5 @@
-use std::path::PathBuf;
-
-use erg_common::config::ErgConfig;
-use erg_common::error::MultiErrorDisplay;
-use erg_common::python_util::PythonVersion;
-use erg_common::spawn::exec_new_thread;
-use erg_common::style::{GREEN, RESET};
-use erg_common::traits::{Runnable, Stream};
-
-use erg_compiler::error::CompileErrors;
-
-use erg::DummyVM;
+mod common;
+use common::{expect_end_with, expect_failure, expect_success};
 
 #[test]
 fn exec_addition_ok() -> Result<(), ()> {
@@ -213,74 +203,4 @@ fn exec_side_effect() -> Result<(), ()> {
 #[test]
 fn exec_subtyping() -> Result<(), ()> {
     expect_failure("tests/should_err/subtyping.er", 2)
-}
-
-fn expect_success(file_path: &'static str) -> Result<(), ()> {
-    match exec_vm(file_path) {
-        Ok(0) => Ok(()),
-        Ok(i) => {
-            println!("err: end with {i}");
-            Err(())
-        }
-        Err(errs) => {
-            errs.fmt_all_stderr();
-            Err(())
-        }
-    }
-}
-
-fn expect_end_with(file_path: &'static str, code: i32) -> Result<(), ()> {
-    match exec_vm(file_path) {
-        Ok(0) => Err(()),
-        Ok(i) => {
-            if i == code {
-                Ok(())
-            } else {
-                println!("err: end with {i}");
-                Err(())
-            }
-        }
-        Err(errs) => {
-            errs.fmt_all_stderr();
-            Err(())
-        }
-    }
-}
-
-fn expect_failure(file_path: &'static str, errs_len: usize) -> Result<(), ()> {
-    match exec_vm(file_path) {
-        Ok(0) => Err(()),
-        Ok(_) => Ok(()),
-        Err(errs) => {
-            errs.fmt_all_stderr();
-            if errs.len() == errs_len {
-                Ok(())
-            } else {
-                println!("err: error length is not {errs_len} but {}", errs.len());
-                Err(())
-            }
-        }
-    }
-}
-
-/// The test is intend to run only on 3.11 for fast execution.
-/// To execute on other versions, change the version and magic number.
-fn _exec_vm(file_path: &'static str) -> Result<i32, CompileErrors> {
-    println!("{GREEN}[test] exec {file_path}{RESET}");
-    let mut cfg = ErgConfig::with_main_path(PathBuf::from(file_path));
-    cfg.py_command = if cfg!(windows) {
-        Some("python")
-    } else {
-        Some("python3")
-    };
-    // cfg.target_version = Some(PythonVersion::new(3, Some(10), Some(6))); // your Python's version
-    // cfg.py_magic_num = Some(3439); // in (most) 3.10.x
-    cfg.target_version = Some(PythonVersion::new(3, Some(11), Some(0)));
-    cfg.py_magic_num = Some(3495); // in 3.11.0
-    let mut vm = DummyVM::new(cfg);
-    vm.exec()
-}
-
-fn exec_vm(file_path: &'static str) -> Result<i32, CompileErrors> {
-    exec_new_thread(move || _exec_vm(file_path))
 }
