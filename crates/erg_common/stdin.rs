49ca99f5720b46e6ae5999f5c5b7556a8ab1e691
GreasrySlug <9619abgoni@gmai.com>
2023-01-31 09:12:33 +0900

    fix: `#26`
    Arrow keys could not be used on Linux
    so, add REPL input handling by `crossterm`
    Improvement of detailed usability
    - possible to use arrow keys on Linux
    - Cursor shape change block to line
    - Ctrl+c is disable
    - Ctrl+z is the exit key command

--- crates/erg_common/stdin.rs
+++ crates/erg_common/stdin.rs
@@ -1,6 +1,16 @@
 use std::cell::RefCell;
-use std::io::{stdin, BufRead, BufReader};
+use std::process::Command;
+use std::process::Output;
 use std::thread::LocalKey;
+
+use crossterm::{
+    cursor::{MoveToColumn, SetCursorStyle},
+    event::{read, Event, KeyCode, KeyEvent, KeyModifiers},
+    execute,
+    style::Print,
+    terminal::{disable_raw_mode, enable_raw_mode},
+    terminal::{Clear, ClearType},
+};
 
 /// e.g.
 /// ```erg
@@ -21,13 +31,134 @@ pub struct StdinReader {
 
 impl StdinReader {
     pub fn read(&mut self) -> String {
-        let mut buf = "".to_string();
-        let stdin = stdin();
-        let mut reader = BufReader::new(stdin.lock());
-        reader.read_line(&mut buf).unwrap();
+        enable_raw_mode().unwrap();
+        let mut output = std::io::stdout();
+        execute!(output, SetCursorStyle::BlinkingBar).unwrap();
+        let mut line = String::new();
+        self.input(&mut line).unwrap();
+        if line.is_empty() {
+            return "".to_string();
+        }
+        disable_raw_mode().unwrap();
+        execute!(output, SetCursorStyle::DefaultUserShape).unwrap();
         self.lineno += 1;
-        self.buf.push(buf);
+        self.buf.push(line);
         self.buf.last().cloned().unwrap_or_default()
+    }
+
+    fn input(&mut self, line: &mut String) -> std::io::Result<()> {
+        let mut position = 0;
+        let mut consult_history = false;
+        let mut stdout = std::io::stdout();
+        while let Event::Key(KeyEvent {
+            code, modifiers, ..
+        }) = read()?
+        {
+            consult_history = false;
+            match (code, modifiers) {
+                (KeyCode::Char('z'), KeyModifiers::CONTROL) => {
+                    println!();
+                    line.clear();
+                    line.push_str(":exit");
+                    return Ok(());
+                }
+                (KeyCode::Tab, _) => {
+                    line.insert_str(position, "    ");
+                    position += 4;
+                }
+                (KeyCode::Home, _) => {
+                    position = 0;
+                }
+                (KeyCode::End, _) => {
+                    position = line.len();
+                }
+                (KeyCode::Backspace, _) => {
+                    if position == 0 {
+                        continue;
+                    }
+                    line.remove(position - 1);
+                    position -= 1;
+                }
+                (KeyCode::Delete, _) => {
+                    if position == line.len() {
+                        continue;
+                    }
+                    line.remove(position);
+                }
+                (KeyCode::Up, _) => {
+                    consult_history = true;
+                    if self.history_input_position == 0 {
+                        continue;
+                    }
+                    self.history_input_position -= 1;
+                    execute!(stdout, MoveToColumn(4), Clear(ClearType::UntilNewLine))?;
+                    if let Some(l) = self.buf.get(self.history_input_position) {
+                        position = l.len();
+                        line.clear();
+                        line.push_str(l);
+                    }
+                }
+                (KeyCode::Down, _) => {
+                    if self.history_input_position == self.buf.len() {
+                        continue;
+                    }
+                    if self.history_input_position == self.buf.len() - 1 {
+                        *line = "".to_string();
+                        position = 0;
+                        self.history_input_position += 1;
+                        execute!(
+                            stdout,
+                            MoveToColumn(4),
+                            Clear(ClearType::UntilNewLine),
+                            MoveToColumn(self.indent * 4),
+                            Print(line.to_owned()),
+                            MoveToColumn(self.indent * 4 + position as u16)
+                        )?;
+                        continue;
+                    }
+                    self.history_input_position += 1;
+                    execute!(stdout, MoveToColumn(4), Clear(ClearType::UntilNewLine))?;
+                    if let Some(l) = self.buf.get(self.history_input_position) {
+                        position = l.len();
+                        line.clear();
+                        line.push_str(l);
+                    }
+                }
+                (KeyCode::Left, _) => {
+                    if position == 0 {
+                        continue;
+                    }
+                    position -= 1;
+                }
+                (KeyCode::Right, _) => {
+                    if position == line.len() {
+                        continue;
+                    }
+                    position += 1;
+                }
+                (KeyCode::Enter, _) => {
+                    println!();
+                    break;
+                }
+                (KeyCode::Char(c), _) => {
+                    line.insert(position, c);
+                    position += 1;
+                }
+                _ => {}
+            }
+            execute!(
+                stdout,
+                MoveToColumn(4),
+                Clear(ClearType::UntilNewLine),
+                MoveToColumn(self.indent * 4),
+                Print(line.to_owned()),
+                MoveToColumn(self.indent * 4 + position as u16)
+            )?;
+        }
+        if !consult_history {
+            self.history_input_position = self.buf.len() + 1;
+        }
+        Ok(())
     }
 
     pub fn reread(&self) -> String {
