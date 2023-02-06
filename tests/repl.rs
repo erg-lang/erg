735e15760498935b69bef080b041ab63f924052c
GreasrySlug <9619abgoni@gmai.com>
2023-02-06 10:58:41 +0900

    test(REPL): check at-sign and class block

--- tests/repl.rs
+++ tests/repl.rs
@@ -63,6 +63,62 @@ fn exec_repl_auto_indent_dedent_check() -> Result<(), ()> {
 }
 
 #[test]
+fn exec_repl_class_def() -> Result<(), ()> {
+    expect_repl_success(
+        "repl_auto_indent_dedent",
+        [
+            "C = Class()",
+            "C.",
+            "attr = 1",
+            "",
+            "print! C.attr",
+            ":exit",
+        ]
+        .into_iter()
+        .map(|line| line.to_string())
+        .collect(),
+    )
+}
+
+#[test]
+fn exec_repl_class_def_with_deco() -> Result<(), ()> {
+    expect_repl_success(
+        "repl_auto_indent_dedent",
+        [
+            "@Inheritable",
+            "C = Class{ x = Int }",
+            "C.",
+            "attr = 1",
+            "",
+            "print! C.attr",
+            ":exit",
+        ]
+        .into_iter()
+        .map(|line| line.to_string())
+        .collect(),
+    )
+}
+
+#[test]
+fn exec_invalid_class_def() -> Result<(), ()> {
+    expect_repl_failure(
+        "repl_auto_indent_dedent",
+        [
+            "C = a Class() # Invalid but pass the expect block",
+            "C.",
+            "attr = 1",
+            "",
+            "print! C.attr",
+            ":exit",
+        ]
+        .into_iter()
+        .map(|line| line.to_string())
+        .collect(),
+        2,
+    )
+}
+
+#[test]
 fn exec_repl_invalid_indent() -> Result<(), ()> {
     expect_repl_failure(
         "repl_invalid_indent",
@@ -81,5 +137,17 @@ fn exec_repl_invalid_indent() -> Result<(), ()> {
         .map(|x| x.to_string())
         .collect(),
         3,
+    )
+}
+
+#[test]
+fn exec_repl_invalid_def_after_the_at_sign() -> Result<(), ()> {
+    expect_repl_failure(
+        "repl_invalid_indent",
+        ["@decorator", "a = 1", "exit()"]
+            .into_iter()
+            .map(|x| x.to_string())
+            .collect(),
+        1,
     )
 }
