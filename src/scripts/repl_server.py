a97316a11471d7d6273165722d5c27ecfd9e1229
Hanaasagi <ambiguous404@gmail.com>
2023-04-25 02:22:35 +0900

    feat: impl message stream in Python server

--- src/scripts/repl_server.py
+++ src/scripts/repl_server.py
@@ -5,15 +5,6 @@ import importlib
 import io
 import traceback
 
-server_socket = socket.socket()
-# DummyVM will replace this __PORT__ with free port
-server_socket.bind(('127.0.0.1', __PORT__))
-server_socket.listen(1)
-(client_socket, client_address) = server_socket.accept()
-
-already_loaded = False
-ctx = {'importlib': importlib}
-
 class INST:
     # Informs that it is not a supported instruction.
     UNKNOWN = 0x00
@@ -28,20 +19,55 @@ class INST:
     # Informs that the connection is to be / should be terminated.
     EXIT = 0x05
 
-def encode(instr, data=''):
-    data_bytes = data.encode()
-    data_len = len(data_bytes)
-    # one byte for inst, two bytes for size, and n bytes for data(Optional)
-    return instr.to_bytes(1, 'big') + data_len.to_bytes(2, 'big') + data_bytes
+class MessageStream:
+    def __init__(self, socket):
+        self.socket = socket
+        self._read_buf = bytearray()
+
+    def recv_msg(self):
+        self._read_buf.clear()
+        # requires at least 3 bytes as metadata
+        while len(self._read_buf) < 3:
+            self._read_buf.extend(self.socket.recv(1024))
+
+        inst = int.from_bytes(self._read_buf[:1], 'big')
+        data_len = int.from_bytes(self._read_buf[1:3], 'big')
+
+        # until all data has been read
+        while len(self._read_buf) < 3 + data_len:
+            self._read_buf.extend(self.socket.recv(1024))
+
+        return (inst, self._read_buf[3:].decode())
+
+
+    def send_msg(self, inst, data=''):
+        data_bytes = data.encode()
+        data_len = len(data_bytes)
+        # one byte for inst, two bytes for size, and n bytes for data(Optional)
+        raw_bytes = inst.to_bytes(1, 'big') + data_len.to_bytes(2, 'big') + data_bytes
+
+        self.socket.send(raw_bytes)
+
+    def close():
+        self.socket.close()
+
+server_socket = socket.socket()
+# DummyVM will replace this __PORT__ with free port
+server_socket.bind(('127.0.0.1', __PORT__))
+server_socket.listen(1)
+(client_socket, client_address) = server_socket.accept()
+
+already_loaded = False
+ctx = {'importlib': importlib}
+client_stream = MessageStream(client_socket)
 
 while True:
     try:
-        data = client_socket.recv(1024)
+        inst, _ = client_stream.recv_msg()
     except ConnectionResetError: # when the server was crashed
         break
-    inst = int.from_bytes(data[:1], 'big')
     if inst == INST.EXIT: # when the server was closed successfully
-        client_socket.send(encode(INST.EXIT))
+        client_stream.send_msg(INST.EXIT)
         break
     elif inst == INST.LOAD:
         sys.stdout = io.StringIO()
@@ -57,7 +83,7 @@ while True:
                 res = str(exec('import __MODULE__', ctx))
             already_loaded = True
         except SystemExit:
-            client_socket.send(encode(INST.EXCEPTION, 'SystemExit'))
+            client_stream.send_msg(INST.EXCEPTION, 'SystemExit')
             continue
         except Exception as e:
             try:
@@ -72,9 +98,9 @@ while True:
             out += '\n'
         res = out + exc + res
         buf.append(res)
-        client_socket.send(encode(resp_inst, ''.join(buf)))
+        client_stream.send_msg(resp_inst, ''.join(buf))
     else:
-        client_socket.send(encode(INST.UNKNOWN))
+        client_stream.send_msg(INST.UNKNOWN)
 
-client_socket.close()
+client_stream.close()
 server_socket.close()
