4f8146875cb99ae00b984ff4c1cff744f8c1cdc8
Shunsuke Shibayama <sbym1346@gmail.com>
2023-04-25 00:33:03 +0900

    refactor: delete unnecessary double escapes

--- src/scripts/repl_server.py
+++ src/scripts/repl_server.py
@@ -1,19 +1,18 @@
-# Append __ to all variables to prevent name collisions in exec
 # All strings must be quoted by single quotes to prevent shell interpretation
-import socket as __socket
-import sys as __sys
-import importlib as __importlib
-import io as __io
-import traceback as __traceback
+import socket
+import sys
+import importlib
+import io
+import traceback
 
-__server_socket = __socket.socket()
+server_socket = socket.socket()
 # DummyVM will replace this __PORT__ with free port
-__server_socket.bind(('127.0.0.1', __PORT__))
-__server_socket.listen(1)
-(__client_socket, __client_address) = __server_socket.accept()
+server_socket.bind(('127.0.0.1', __PORT__))
+server_socket.listen(1)
+(client_socket, client_address) = server_socket.accept()
 
-__already_loaded = False
-__ctx = {'__importlib': __importlib}
+already_loaded = False
+ctx = {'importlib': importlib}
 
 class INST:
     # Informs that it is not a supported instruction.
@@ -29,7 +28,7 @@ class INST:
     # Informs that the connection is to be / should be terminated.
     EXIT = 0x05
 
-def __encode(instr, data=''):
+def encode(instr, data=''):
     data_bytes = data.encode()
     data_len = len(data_bytes)
     # one byte for inst, two bytes for size, and n bytes for data(Optional)
@@ -37,45 +36,45 @@ def __encode(instr, data=''):
 
 while True:
     try:
-        __data = __client_socket.recv(1024)
+        data = client_socket.recv(1024)
     except ConnectionResetError: # when the server was crashed
         break
-    __inst = int.from_bytes(__data[:1], 'big')
-    if __inst == INST.EXIT: # when the server was closed successfully
-        __client_socket.send(__encode(INST.EXIT))
+    inst = int.from_bytes(data[:1], 'big')
+    if inst == INST.EXIT: # when the server was closed successfully
+        client_socket.send(encode(INST.EXIT))
         break
-    elif __inst == INST.LOAD:
-        __sys.stdout = __io.StringIO()
-        __res = ''
-        __exc = ''
-        __resp_inst = INST.PRINT
-        __buf = []
+    elif inst == INST.LOAD:
+        sys.stdout = io.StringIO()
+        res = ''
+        exc = ''
+        resp_inst = INST.PRINT
+        buf = []
         try:
-            if __already_loaded:
+            if already_loaded:
                 # __MODULE__ will be replaced with module name
-                __res = str(exec('__importlib.reload(__MODULE__)', __ctx))
+                res = str(exec('importlib.reload(__MODULE__)', ctx))
             else:
-                __res = str(exec('import __MODULE__', __ctx))
-            __already_loaded = True
+                res = str(exec('import __MODULE__', ctx))
+            already_loaded = True
         except SystemExit:
-            __client_socket.send(__encode(INST.EXCEPTION, 'SystemExit'))
+            client_socket.send(encode(INST.EXCEPTION, 'SystemExit'))
             continue
         except Exception as e:
             try:
-                excs = __traceback.format_exception(e)
+                excs = traceback.format_exception(e)
             except:
-                excs = __traceback.format_exception_only(e.__class__, e)
-            __exc = ''.join(excs).rstrip()
-            __traceback.clear_frames(e.__traceback__)
-            __resp_inst = INST.INITIALIZE
-        __out = __sys.stdout.getvalue()[:-1]
-        if __out and __exc or __res:
-            __out += '\n'
-        __res = __out + __exc + __res
-        __buf.append(__res)
-        __client_socket.send(__encode(__resp_inst, ''.join(__buf)))
+                excs = traceback.format_exception_only(e.__class__, e)
+            exc = ''.join(excs).rstrip()
+            traceback.clear_frames(e.__traceback__)
+            resp_inst = INST.INITIALIZE
+        out = sys.stdout.getvalue()[:-1]
+        if out and exc or res:
+            out += '\n'
+        res = out + exc + res
+        buf.append(res)
+        client_socket.send(encode(resp_inst, ''.join(buf)))
     else:
-        __client_socket.send(__encode(INST.UNKNOWN))
+        client_socket.send(encode(INST.UNKNOWN))
 
-__client_socket.close()
-__server_socket.close()
+client_socket.close()
+server_socket.close()
