1696c6ec76ba7c69faf642e8515f4c9f61fbf80f
GreasySlug <9619abgoni@gmail.com>
2023-05-13 15:38:37 +0900

    feat(Ratio): add new methods

--- crates/erg_common/ratio.rs
+++ crates/erg_common/ratio.rs
@@ -1,6 +1,6 @@
 use std::cmp::Ordering::{self, Equal, Greater, Less};
 use std::fmt::Display;
-use std::ops::{Add, Div, Mul, Sub};
+use std::ops::{Add, Div, Mul, Rem, Sub};
 
 #[derive(Debug, Eq, PartialEq, Clone, Copy, Hash)]
 pub struct Ratio {
@@ -9,6 +9,7 @@ pub struct Ratio {
 }
 
 impl Ratio {
+    #[inline]
     pub fn new(numer: i64, denom: i64) -> Self {
         if numer == 0 {
             return Self::zero();
@@ -24,11 +25,13 @@ impl Ratio {
         }
     }
 
+    #[inline]
     pub fn zero() -> Self {
         Self { numer: 0, denom: 1 }
     }
 
     const EPSILON: f64 = 1e-10;
+    #[inline]
     pub fn float_new(f: f64, limit: Option<u64>) -> Self {
         let mut f = f;
         let mut minus = false;
@@ -73,6 +76,7 @@ impl Ratio {
         Ratio::new(n, d)
     }
 
+    #[inline]
     pub fn to_float(self) -> f64 {
         self.numer as f64 / self.denom as f64
     }
@@ -82,6 +86,31 @@ impl Ratio {
     }
     pub fn numer(&self) -> i64 {
         self.numer
+    }
+
+    #[inline]
+    pub fn floor(&self) -> Self {
+        if *self < Ratio::zero() {
+            Ratio::new((self.numer - self.denom + 1) / self.denom, 1)
+        } else {
+            Ratio::new(self.numer / self.denom, 1)
+        }
+    }
+
+    #[inline]
+    pub fn ceil(&self) -> Self {
+        if *self < Ratio::zero() {
+            Ratio::new(self.numer / self.denom, 1)
+        } else {
+            Ratio::new((self.numer + self.denom - 1) / self.denom, 1)
+        }
+    }
+
+    #[inline]
+    pub fn pow(&self, n: u32) -> Self {
+        let numer = self.numer.pow(n);
+        let denom = self.denom.pow(n);
+        Self::new(numer, denom)
     }
 }
 
@@ -121,9 +150,19 @@ impl Div for Ratio {
     type Output = Self;
 
     fn div(self, rhs: Self) -> Self::Output {
-        let numer = self.numer * rhs.denom;
-        let denom = self.denom * rhs.numer;
+        let numer = self.numer / rhs.denom;
+        let denom = self.denom / rhs.numer;
         Self::new(numer, denom)
+    }
+}
+
+impl Rem for Ratio {
+    type Output = Self;
+
+    fn rem(self, rhs: Self) -> Self::Output {
+        let sd = self.denom;
+        let od = rhs.denom;
+        Ratio::new((self.numer * od) % (rhs.numer * sd), sd * od)
     }
 }
 
@@ -176,6 +215,9 @@ fn lcm(x: i64, y: i64) -> i64 {
 
 #[cfg(test)]
 mod test {
+
+    use std::f64::consts::PI;
+
     use super::Ratio;
     use crate::ratio::{gcd, lcm};
 
@@ -286,5 +328,76 @@ mod test {
         assert_eq!(Ratio::new(1, 3), a);
         let a = Ratio::float_new(1.47, None);
         assert_eq!(Ratio::new(147, 100), a);
+    }
+
+    #[test]
+    fn test_floor() {
+        let a = Ratio::new(1, 3);
+        assert_eq!(Ratio::new(0, 0), a.floor());
+        let a = Ratio::new(100, 3);
+        assert_eq!(Ratio::new(33, 1), a.floor());
+        let a = Ratio::new(-100, 3);
+        assert_eq!(Ratio::new(-33, 1), a.floor());
+        let a = Ratio::float_new(10.0, None);
+        assert_eq!(10.0, a.floor().to_float());
+        let a = Ratio::float_new(-0.8, None);
+        assert_eq!(-1.0, a.floor().to_float());
+    }
+
+    #[test]
+    fn test_ceil() {
+        let a = Ratio::new(1, 3);
+        assert_eq!(Ratio::new(1, 1), a.ceil());
+        let a = Ratio::new(100, 3);
+        assert_eq!(Ratio::new(34, 1), a.ceil());
+        let a = Ratio::new(-100, 3);
+        assert_eq!(Ratio::new(-32, 1), a.ceil());
+        let a = Ratio::float_new(10.5, None);
+        assert_eq!(11.0, a.ceil().to_float());
+    }
+
+    #[test]
+    fn test_pow() {
+        let a = Ratio::new(1, 3);
+        assert_eq!(Ratio::new(1, 9), a.pow(2));
+        assert_eq!(Ratio::new(1, 27), a.pow(3));
+        let a = Ratio::new(1, 2);
+        assert_eq!(Ratio::new(1, 1024), a.pow(10));
+        let a = Ratio::new(1, -4);
+        assert_eq!(Ratio::new(1, 16), a.pow(2));
+        assert_eq!(Ratio::new(-1, 64), a.pow(3));
+    }
+
+    #[test]
+    fn test_rem() {
+        let a = Ratio::float_new(100.0, None);
+        let b = Ratio::float_new(20.0, None);
+        assert_eq!(Ratio::zero(), a % b);
+        let a = Ratio::float_new(3.0, None);
+        let b = Ratio::float_new(2.0, None);
+        assert_eq!(Ratio::new(1, 1), a % b);
+        let a = Ratio::float_new(3.5, None);
+        let b = Ratio::float_new(2.2, None);
+        assert_eq!(Ratio::new(13, 10), a % b);
+    }
+
+    #[test]
+    fn test_limited_float_new() {
+        let a = Ratio::float_new(PI, Some(10));
+        assert_eq!(Ratio::new(10, 3), a);
+        let a = Ratio::float_new(PI, Some(100));
+        assert_eq!(Ratio::new(78, 25), a);
+        let a = Ratio::float_new(PI, Some(1_000));
+        assert_eq!(Ratio::new(761, 242), a);
+        let a = Ratio::float_new(PI, Some(10_000));
+        assert_eq!(Ratio::new(7587, 2416), a);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_overflow() {
+        let a = Ratio::new(i64::MAX, 1);
+        let b = Ratio::new(1, 1);
+        let _ = a + b;
     }
 }
