41d8ca9cf65958a35780e1ed6f8171cfe08926c3
Shunsuke Shibayama <sbym1346@gmail.com>
2025-01-21 14:52:52 +0900

    Update serialize.rs

--- crates/erg_common/serialize.rs
+++ crates/erg_common/serialize.rs
@@ -34,7 +34,7 @@ pub const fn get_ver_from_magic_num(magic_num: u32) -> PythonVersion {
         3430..=3439 => PythonVersion::new(3, Some(10), Some(0)), // main: 3439
         3495 => PythonVersion::new(3, Some(11), Some(0)),
         3531 => PythonVersion::new(3, Some(12), Some(0)),
-        _ => panic!("unknown magic number"),
+        _ => panic!("unknown magic number (unsupported Python version)"),
     }
 }
 
